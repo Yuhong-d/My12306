85a25feadaf17202f163f78fcb5c3e1daa9ca0c6
xiaoxingOvO <793408755@qq.com>
2020-07-01 15:36:27 +0800

    测试

--- app/src/main/java/com/example/abc123/my12306/User/my_contact.java
+++ app/src/main/java/com/example/abc123/my12306/User/my_contact.java
@@ -3,14 +3,34 @@ package com.example.abc123.my12306.User;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.example.abc123.my12306.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class my_contact extends AppCompatActivity {
+    private ListView listView;
+    private SimpleAdapter adapter;
+    private List<Map<String,Object>> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_contact);
+        listView = findViewById(R.id.Lv_detailcontact);
+        data =new ArrayList<>();
+        Map<String,Object> row = new HashMap<>();
+        row.put("name", "东方人(成人)");
+        row.put("idCard", "身份证:110110199009091111");
+        row.put("tel", "电话:138888888899");
+        data.add(row);
+        adapter = new SimpleAdapter(this,data,R.layout.account_list_item,new String[]{"name","idCard","tel"},
+                new int[]{ R.id.tvNameContact,  R.id.tvIdCardContact, R.id.tvTelContact });
+        listView.setAdapter(adapter);
     }
 }
