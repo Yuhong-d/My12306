0ed9d51619875dc16387799eba5483e429100823
xiaoxingOvO <793408755@qq.vom>
2020-08-24 11:28:14 +0800

    8.23-18:40

---
+++ app/src/main/java/com/example/abc123/my12306/util/Constant.java
@@ -0,0 +1,8 @@
+package com.example.abc123.my12306.util;
+
+public class Constant {
+    public static final String Host = "http://192.168.1.3:8080/My12306";
+    public static final int REQUEST_TIMEOUT = 3000;
+    public static final int SO_TIMEOUT = 3000;
+
+}
